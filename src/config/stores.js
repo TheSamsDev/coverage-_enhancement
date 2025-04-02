725a688b7ffa04b6d2ec1ba3ac222a41d50f95b0
samsdev-ai <workspacesam174@gmail.com>
2025-04-03 03:12:50 +0500

    login design change

--- src/config/stores.js
+++ src/config/stores.js
@@ -4,7 +4,7 @@ import { supabase } from './supabase'
 export const getStores = async () => {
     try {
         const { data, error } = await supabase
-            .from('stores')
+            .from('shops')
             .select('*')
         if (error) throw error
         return { data, error: null }
